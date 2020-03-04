ea2e48a4031ad57723a43a0bdef95a94d4d00655
Sergey Omelchuk <sergomel52@gmail.com>
2020-03-04 19:06:20 +0100

    Added default toStream() method to both INode and ICompositeNode. Used stream flattering for nested nodes.

--- src/main/java/interfaces/ICompositeNode.java
+++ src/main/java/interfaces/ICompositeNode.java
@@ -1,7 +1,14 @@
 package interfaces;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface ICompositeNode extends INode {
     List<INode> getNodes();
+
+    @Override
+    default Stream<INode> toStream() {
+        return Stream.concat(INode.super.toStream(),
+                this.getNodes().stream().flatMap(INode::toStream));
+    }
 }
--- src/main/java/interfaces/INode.java
+++ src/main/java/interfaces/INode.java
@@ -1,6 +1,12 @@
 package interfaces;
 
+import java.util.stream.Stream;
+
 public interface INode {
     String getNode();
     String getRenderer();
+
+    default Stream<INode> toStream() {
+        return Stream.of(this);
+    }
 }
