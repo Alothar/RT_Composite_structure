4e85d2af7489858067319c497daad3de551b0504
Sergey Omelchuk <sergomel52@gmail.com>
2020-03-04 20:08:55 +0100

    Added simple test for blank array count test. Added tests for findByCode method.

--- src/test/java/MyStructureTest.java
+++ src/test/java/MyStructureTest.java
@@ -6,6 +6,7 @@ import interfaces.INode;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 
@@ -13,12 +14,17 @@ public class MyStructureTest {
 
     public INode node = new Node("Node", "Renderer");
     public INode node1 = new Node("Node1", "Renderer1");
+    public INode node_nulls = new Node(null, null);
     public ICompositeNode compositeNode = new CompositeNode("CompositeNode", "CompositeRenderer", Arrays.asList(node, node1));
     public ICompositeNode compositeNode1 = new CompositeNode("CompositeNode1", "CompositeRenderer1", Arrays.asList(node, node1, compositeNode));
     public MyStructure structure = new MyStructure(null);
 
     @Test
     public void countTest_NoNodesPresent_mustReturnZero(){
+        Assertions.assertEquals(0, structure.count());
+
+        structure = new MyStructure(new ArrayList<>());
+
         Assertions.assertEquals(0, structure.count());
     }
 
@@ -41,5 +47,38 @@ public class MyStructureTest {
         structure = new MyStructure(Arrays.asList(node, node1, compositeNode, compositeNode1));
 
         Assertions.assertEquals(11, structure.count());
+    }
+
+    @Test
+    public void findByCodeTest_NoNodesPresent_mustReturnNulls(){
+        Assertions.assertNull(structure.findByCode(null));
+        Assertions.assertNull(structure.findByCode(""));
+        Assertions.assertNull(structure.findByCode(" "));
+    }
+
+    @Test
+    public void findByCodeTest_OnlySimpleNodesPresent_mustReturnSimpleNodes(){
+        structure = new MyStructure(Arrays.asList(node, node1, node_nulls));
+
+        Assertions.assertEquals(node_nulls, structure.findByCode(null));
+        Assertions.assertEquals(node, structure.findByCode("Node"));
+        Assertions.assertEquals(node1, structure.findByCode("Node1"));
+    }
+
+    @Test
+    public void findByCodeTest_ComplexPresent_mustReturnSimpleNodes(){
+        structure = new MyStructure(Arrays.asList(compositeNode, node, node1, node_nulls));
+
+        Assertions.assertEquals(node_nulls, structure.findByCode(null));
+        Assertions.assertEquals(node, structure.findByCode("Node"));
+        Assertions.assertEquals(node1, structure.findByCode("Node1"));
+    }
+
+    @Test
+    public void findByCodeTest_ComplexPresent_mustReturnComplexNodes(){
+        structure = new MyStructure(Arrays.asList(compositeNode, compositeNode1, node, node1, node_nulls));
+
+        Assertions.assertEquals(compositeNode, structure.findByCode("CompositeNode"));
+        Assertions.assertEquals(compositeNode1, structure.findByCode("CompositeNode"));
     }
 }
