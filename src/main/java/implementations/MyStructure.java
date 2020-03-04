Alothar/RT_Composite_structure

commit 4f73ccfd8713b7c2e2489bab5e048df17ecc733b

Replaced null check with built-in isEmpty().

src/main/java/implementations/MyStructure.java
@@ -22,7 +22,7 @@ public class MyStructure implements IMyStructure {
     }
 
     public int count() {
-        if(nodes==null)
+        if(nodes.isEmpty())
             return 0;
         return (int)nodes.stream().flatMap(INode::toStream).count();
     }

commit b61c7d648bb7d461f117c6edb920c88612e85d43

Fixed bug introduced in 4f73ccfd.

src/main/java/implementations/MyStructure.java
@@ -22,7 +22,7 @@ public class MyStructure implements IMyStructure {
     }
 
     public int count() {
-        if(nodes.isEmpty())
+        if(nodes == null)
             return 0;
         return (int)nodes.stream().flatMap(INode::toStream).count();
     }