293d1a5abf6b6021d50f97fac9dc9fc4733750ca
Sergey Omelchuk <sergomel52@gmail.com>
2020-03-05 21:47:53 +0100

    Added simple tests for findByRenderer method.

--- src/test/java/MyStructureTest.java
+++ src/test/java/MyStructureTest.java
@@ -81,4 +81,37 @@ public class MyStructureTest {
         Assertions.assertEquals(compositeNode, structure.findByCode("CompositeNode"));
         Assertions.assertEquals(compositeNode1, structure.findByCode("CompositeNode1"));
     }
+
+    @Test
+    public void findByRendererTest_NoNodesPresent_mustReturnNull(){
+        structure = new MyStructure(new ArrayList<>());
+
+        Assertions.assertNull(structure.findByCode(null));
+    }
+
+    @Test
+    public void findByRendererTest_OnlySimpleNodesPresent_mustReturnSimpleNodes(){
+        structure = new MyStructure(Arrays.asList(node, node1, node_nulls));
+
+        Assertions.assertEquals(node_nulls, structure.findByCode(null));
+        Assertions.assertEquals(node, structure.findByCode("Renderer"));
+        Assertions.assertEquals(node1, structure.findByCode("Renderer1"));
+    }
+
+    @Test
+    public void findByRendererTest_ComplexPresent_mustReturnSimpleNodes(){
+        structure = new MyStructure(Arrays.asList(compositeNode, node, node1, node_nulls));
+
+        Assertions.assertEquals(node_nulls, structure.findByCode(null));
+        Assertions.assertEquals(node, structure.findByCode("Renderer"));
+        Assertions.assertEquals(node1, structure.findByCode("Renderer1"));
+    }
+
+    @Test
+    public void findByRendererTest_ComplexPresent_mustReturnComplexNodes(){
+        structure = new MyStructure(Arrays.asList(compositeNode, compositeNode1, node, node1, node_nulls));
+
+        Assertions.assertEquals(compositeNode, structure.findByCode("CompositeRenderer"));
+        Assertions.assertEquals(compositeNode1, structure.findByCode("CompositeRenderer1"));
+    }
 }
